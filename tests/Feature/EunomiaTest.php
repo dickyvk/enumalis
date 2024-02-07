83277f55222de77df0462f5333899392b8e1d92d
Dicky VK <d.valentiusk@gmail.com>
2024-02-07 11:55:05 +0700

    Testing dipisah per fungsinya

--- tests/Feature/EunomiaTest.php
+++ tests/Feature/EunomiaTest.php
@@ -11,9 +11,8 @@ use App\Models\Rule;
 
 class EunomiaTest extends TestCase
 {
-    public function test_user_successful()
+    public function test_user_register()
     {
-        //Store
         $payload = [
             'uid' => Str::random(35),
             'name' => fake()->name(),
@@ -33,9 +32,15 @@ class EunomiaTest extends TestCase
                 ],
             ]);
 
-        //Login
+        $user = User::where('uid', $payload['uid'])->first();
+        $user->delete();
+    }
+    public function test_user_login()
+    {
+        $user = User::factory()->create();
+
         $payload = [
-            'uid' => $payload['uid'],
+            'uid' => $user->uid,
         ];
         $this->json('POST', 'eunomia/login', $payload)
             ->assertStatus(200)
@@ -50,10 +55,14 @@ class EunomiaTest extends TestCase
                 ],
             ]);
 
-        //Show
-        $user = User::where('uid', $payload['uid'])->first();
+        $user->delete();
+    }
+    public function test_user_show()
+    {
+        $user = User::factory()->create();
         $token = $user->createToken('auth_token')->plainTextToken;
         $headers = ['Authorization' => "Bearer $token"];
+
         $this->json('get', 'eunomia', [], $headers)
             ->assertStatus(200)
             ->assertJsonStructure([
@@ -65,7 +74,14 @@ class EunomiaTest extends TestCase
                 'updated_at',
             ]);
 
-        //Update
+        $user->delete();
+    }
+    public function test_user_update()
+    {
+        $user = User::factory()->create();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        $headers = ['Authorization' => "Bearer $token"];
+
         $payload = [
             'name' => fake()->name(),
             'email' => fake()->optional()->safeEmail(),
@@ -74,10 +90,18 @@ class EunomiaTest extends TestCase
         $this->json('post', 'eunomia', $payload, $headers)->assertStatus(200);
         $user = User::where('id', $user->id)->first();
         foreach($payload as $key => $value) {
-            $this->assertEquals($payload[$key], $user->$key);
+            $this->assertEquals($user->$key, $payload[$key]);
         }
 
-        //Rule
+        $user->delete();
+    }
+    public function test_user_rule()
+    {
+        $user = User::factory()->create();
+        $rule = Rule::factory()->create(['users_id' => $user->id]);
+        $token = $user->createToken('auth_token')->plainTextToken;
+        $headers = ['Authorization' => "Bearer $token"];
+
         $payload = [
             'terms' => fake()->numberBetween($min = 0, $max = 1),
             'policy' => fake()->numberBetween($min = 0, $max = 1),
@@ -85,12 +109,32 @@ class EunomiaTest extends TestCase
         $this->json('post', 'eunomia/rule', $payload, $headers)->assertStatus(200);
         $rule = Rule::where('users_id', $user->id)->first();
         foreach($payload as $key => $value) {
-            $this->assertEquals($payload[$key], $rule->$key);
+            $this->assertEquals($rule->$key, $payload[$key]);
         }
-        
-        //Logout
+
+        $user->delete();
+    }
+    public function test_user_logout()
+    {
+        $user = User::factory()->create();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        $headers = ['Authorization' => "Bearer $token"];
+
         $this->json('post', 'eunomia/logout', [], $headers)->assertStatus(204);
         $this->json('get', 'eunomia', [], $headers)->assertStatus(500);
+
+        $user->delete();
+    }
+
+    public function test_master_successful()
+    {
+        $user = User::factory()->create();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        $headers = ['Authorization' => "Bearer $token"];
+
+        $user->type = 1;
+        $user->save();
+        $this->assertEquals($user->type, 'master');
 
         $user->delete();
     }
