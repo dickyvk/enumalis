de41d6b66755adaacb66f0a10d96ba6a7531b345
Dicky VK <d.valentiusk@gmail.com>
2024-02-07 23:39:35 +0700

    Test sampai master sudah selesai

--- tests/Feature/EunomiaTest.php
+++ tests/Feature/EunomiaTest.php
@@ -11,7 +11,7 @@ use App\Models\Rule;
 
 class EunomiaTest extends TestCase
 {
-    public function test_user_register()
+    public function test_register_new_user()
     {
         $payload = [
             'uid' => Str::random(35),
@@ -35,7 +35,7 @@ class EunomiaTest extends TestCase
         $user = User::where('uid', $payload['uid'])->first();
         $user->delete();
     }
-    public function test_user_login()
+    public function test_login_as_user()
     {
         $user = User::factory()->create();
 
@@ -57,7 +57,7 @@ class EunomiaTest extends TestCase
 
         $user->delete();
     }
-    public function test_user_show()
+    public function test_show_user_detail()
     {
         $user = User::factory()->create();
         $token = $user->createToken('auth_token')->plainTextToken;
@@ -76,7 +76,7 @@ class EunomiaTest extends TestCase
 
         $user->delete();
     }
-    public function test_user_update()
+    public function test_update_user_detail()
     {
         $user = User::factory()->create();
         $token = $user->createToken('auth_token')->plainTextToken;
@@ -95,7 +95,7 @@ class EunomiaTest extends TestCase
 
         $user->delete();
     }
-    public function test_user_rule()
+    public function test_update_user_rule()
     {
         $user = User::factory()->create();
         $rule = Rule::factory()->create(['users_id' => $user->id]);
@@ -114,7 +114,7 @@ class EunomiaTest extends TestCase
 
         $user->delete();
     }
-    public function test_user_logout()
+    public function test_logout_user()
     {
         $user = User::factory()->create();
         $token = $user->createToken('auth_token')->plainTextToken;
@@ -126,16 +126,66 @@ class EunomiaTest extends TestCase
         $user->delete();
     }
 
-    public function test_master_successful()
+    public function test_register_new_master()
     {
         $user = User::factory()->create();
-        $token = $user->createToken('auth_token')->plainTextToken;
-        $headers = ['Authorization' => "Bearer $token"];
 
         $user->type = 1;
         $user->save();
+        $user = User::where('id', $user->id)->first();
         $this->assertEquals($user->type, 'master');
 
         $user->delete();
+    }
+    public function test_master_show_all_user()
+    {
+        $user = User::factory()->create();
+        $user->type = 1;
+        $user->save();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        $headers = ['Authorization' => "Bearer $token"];
+
+        $this->json('get', 'eunomia/all', [], $headers)
+            ->assertStatus(200)
+            ->assertJsonIsArray();
+
+        $user->delete();
+    }
+    public function test_master_update_other_user()
+    {
+        $user = User::factory()->create();
+        $master = User::factory()->create();
+        $master->type = 1;
+        $master->save();
+        $token = $master->createToken('auth_token')->plainTextToken;
+        $headers = ['Authorization' => "Bearer $token"];
+
+        $payload = [
+            'name' => fake()->name(),
+            'email' => fake()->optional()->safeEmail(),
+            'phone' => fake()->optional()->phoneNumber(),
+        ];
+        $array = $this->json('post', 'eunomia/'.$user->id, $payload, $headers)->assertStatus(200);
+        $user = User::where('id', $user->id)->first();
+        foreach($payload as $key => $value) {
+            $this->assertEquals($user->$key, $payload[$key]);
+        }
+
+        $user->delete();
+        $master->delete();
+    }
+    public function test_master_delete_other_user()
+    {
+        $user = User::factory()->create();
+        $master = User::factory()->create();
+        $master->type = 1;
+        $master->save();
+        $token = $master->createToken('auth_token')->plainTextToken;
+        $headers = ['Authorization' => "Bearer $token"];
+
+        $array = $this->json('delete', 'eunomia/'.$user->id, [], $headers)->assertStatus(204);
+        $array = $this->json('post', 'eunomia/'.$user->id, [], $headers)->assertStatus(404);
+
+        $master->delete();
     }
 }
